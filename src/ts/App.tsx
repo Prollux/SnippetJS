e54f5da3a65047d08e6523b5b4ee955f4f7814a3
Zach Shold <sholdzb@gmail.com>
2021-06-07 15:49:19 -0500

    adds basic user text field and submit button

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,10 +1,20 @@
-import React, {FC} from 'react'
+import React, {FC, useState} from 'react'
 import "../sass/styles.scss"
 
 const App : FC = () => {
+  const onClick = (e) => {
+    e.preventDefault()
+    console.log(input)
+  }
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
   return (
     <div>
-      <h1>Hello World</h1>
+      <textarea value={input} onChange={e => {setInput(e.target.value)}} />
+      <div>
+        <button onClick={e => {onClick(e)}}>Submit</button>
+      </div>
+      <pre></pre>
     </div>
   )
 }
