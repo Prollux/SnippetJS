58a53a0552f2aa0707b3bfeed934b9201e47dca9
Zach Shold <sholdzb@gmail.com>
2021-06-10 16:58:33 -0500

    parent now abelt send compiled code to iframe, iframe now evaluates code (no error handling currently implemented)

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -5,7 +5,8 @@ import { unpkgRedir } from '../plugins/unpkgRedir'
 import { fetchPlugin } from '../plugins/fetch-plugin'
 
 const App : FC = () => {
-  const ref = useRef<any>();
+  const ref = useRef<any>()
+  const iframe = useRef<any>()
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
 
@@ -25,14 +26,7 @@ const App : FC = () => {
           fetchPlugin(input)
         ]
       })
-
-      setCode(result.outputFiles[0].text)
-
-      try {
-        eval(code)
-      } catch(err) {
-        alert(err)
-      }
+      iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
     }
   }
 
@@ -47,15 +41,29 @@ const App : FC = () => {
     startService()
   }, [])
 
+  
+  const html = `
+  <html>
+    <head />
+      <body>
+        <div id='root'></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data)
+          }, false)
+        </script>
+      </body
+    </html>
+`
+
   return (
     <div>
       <textarea value={input} onChange={e => {setInput(e.target.value)}} />
       <div>
         <button onClick={e => {onClick(e)}}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe src='../codebox.html'></iframe>
-    </div>
+      <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html}/>
+      </div>
   )
 }
 
