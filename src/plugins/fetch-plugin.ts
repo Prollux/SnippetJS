107e414630681d9273098c67f0ce097df57bf6df
Zach Shold <sholdzb@gmail.com>
2021-06-10 12:20:54 -0500

    cleans up fetch-plugin, removes if statement for index.js uses onload with filter instead

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,41 +10,41 @@ export const fetchPlugin = (input:string) => {
     return {
         name: 'fetch-plugin',
         setup(build: esbuild.PluginBuild) {
-            build.onLoad({ filter: /.*/ }, async(args:any) => {
-                if (args.path === 'index.js') {
-                    return {
-                        loader: 'jsx',
-                        contents: input,
-                    }
 
-                } else {
-                    // const cachedItem= await cache.getItem<esbuild.OnLoadResult>(args.path)
-                    // if (cachedItem) {
-                    //     return cachedItem
-                    // }
-
-                    const { data, request } = await axios.get(args.path)
-                    const fileType = args.path.match(/.css$/) ? 'css' : 'jsx'
-                    const escapedData = data
-                        .replace(/\n/g, '')
-                        .replace(/"/g, '\\"')
-                        .replace(/'/g, "\\'")
-                    const contents = fileType === 'css' ?   
-                        `
-                            const style = document.createElement('style');
-                            style.innerText = '${escapedData}';
-                            document.head.appendChild(style)
-                        `
-                         : data
-
-                    const result:esbuild.OnLoadResult = {
-                        loader: 'jsx',
-                        contents,
-                        resolveDir: new URL('./', request.responseURL).pathname
-                    }
-                    await cache.setItem(args.path, result)
-                    return result
+            build.onLoad({ filter: /(^index\.js$)/}, () => {
+                return {
+                    loader: 'jsx',
+                    contents: input,
                 }
+            })
+
+            build.onLoad({ filter: /.*/ }, async(args:any) => {
+                // const cachedItem= await cache.getItem<esbuild.OnLoadResult>(args.path)
+                // if (cachedItem) {
+                //     return cachedItem
+                // }
+
+                const { data, request } = await axios.get(args.path)
+                const fileType = args.path.match(/.css$/) ? 'css' : 'jsx'
+                const escapedData = data
+                    .replace(/\n/g, '')
+                    .replace(/"/g, '\\"')
+                    .replace(/'/g, "\\'")
+                const contents = fileType === 'css' ?   
+                    `
+                        const style = document.createElement('style');
+                        style.innerText = '${escapedData}';
+                        document.head.appendChild(style)
+                    `
+                    : data
+
+                const result:esbuild.OnLoadResult = {
+                    loader: 'jsx',
+                    contents,
+                    resolveDir: new URL('./', request.responseURL).pathname
+                }
+                await cache.setItem(args.path, result)
+                return result
             })
         }
     }
