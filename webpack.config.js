7ed6c6a38ba011338bb4b0829b101dc6cab2706f
Zach Shold <sholdzb@gmail.com>
2021-06-14 16:09:28 -0500

    webpack now properly handles css files

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.(s[ac]ss||css)$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       }
     ]
