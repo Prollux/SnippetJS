69cfd90c0d05eaf6bd9b24ad7e931ed6153d01d8
Zach Shold <sholdzb@gmail.com>
2021-06-14 12:50:23 -0500

    enables text wrapping for editor window

--- src/ts/components/CodeEditor.tsx
+++ src/ts/components/CodeEditor.tsx
@@ -2,7 +2,12 @@ import MonacoEditor from '@monaco-editor/react'
 import React from 'react'
 
 const CodeEditor = () => {
-    return <MonacoEditor height='500px' width='750px' theme='vs-dark' language='javascript'/>
+    return <MonacoEditor height='500px'
+      width='750px'
+      theme='vs-dark'
+      language='javascript' options={{
+          wordWrap:'on'
+      }}/>
 }
 
 export default CodeEditor
