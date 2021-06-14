ae4887a781ae239be130d3cae0f6c581695e7de5
Zach Shold <sholdzb@gmail.com>
2021-06-14 12:42:27 -0500

    import monaco-editor instead of Editor

--- src/ts/components/CodeEditor.tsx
+++ src/ts/components/CodeEditor.tsx
@@ -1,8 +1,8 @@
-import Editor from '@monaco-editor/react'
+import MonacoEditor from '@monaco-editor/react'
 import React from 'react'
 
 const CodeEditor = () => {
-    return <Editor height='500px' width='750px' theme='vs-dark' language='javascript'/>
+    return <MonacoEditor height='500px' width='750px' theme='vs-dark' language='javascript'/>
 }
 
 export default CodeEditor
