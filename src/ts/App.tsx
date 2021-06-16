b0bad0d4c66ed9c3efc65b98039b1ddbe0604d36
Zach Shold <sholdzb@gmail.com>
2021-06-16 11:01:38 -0500

    refactors code panel logic out of app and into CodePanel.jsx (+minor css changes)

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,30 +1,14 @@
-import React, {FC, useState, useEffect, useRef} from 'react'
+import React, { FC } from 'react'
 import "../sass/styles.scss"
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import CodeEditor from './components/CodeEditor'
-import Preview from './components/Preview'
-import bundler from '../bundler/index'
+import CodePanel from './components/CodePanel'
+
 
 const App = () => {
-  const ref = useRef<any>()
-  const iframe = useRef<any>()
-  const [input, setInput] = useState('')
-  const [code, setCode] = useState('')
-
-  const onClick = async (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault()
-    const bundle = await bundler(input)
-    setCode(bundle)
-  }
-
   return (
     <>
-    <div id='preview-container'>
-      <CodeEditor value={input} onChange={(value:string)=> setInput(value)} />
-      <Preview code={code} />
-      </div>
-      <button id='code-submit' onClick={e => {onClick(e)}}>Submit</button>
-</>
+    <CodePanel />
+    </>
   )
 }
 
---
+++ src/ts/components/CodePanel.tsx
@@ -0,0 +1,29 @@
+import React, {FC, useState, useEffect, useRef} from 'react'
+import CodeEditor from './CodeEditor'
+import Preview from './Preview'
+import bundler from '../../bundler/index'
+
+const CodePanel = () => {
+  const ref = useRef<any>()
+  const iframe = useRef<any>()
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+
+  const onClick = async (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    const bundle = await bundler(input)
+    setCode(bundle)
+  }
+
+  return (
+    <div className='code-panel'>
+    <div id='preview-container'>
+      <CodeEditor value={input} onChange={(value:string)=> setInput(value)} />
+      <Preview code={code} />
+      </div>
+      <button id='code-submit' onClick={e => {onClick(e)}}>Submit</button>
+</div>
+  )
+}
+
+export default CodePanel
