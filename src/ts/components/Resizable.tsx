f050ded4b88be96e8fdb7b4e22b699af133aa3fd
Zach Shold <sholdzb@gmail.com>
2021-06-17 16:21:51 -0500

    adds internal prop logic for vertical and horizontal to Resizable component

--- src/ts/components/Resizable.tsx
+++ src/ts/components/Resizable.tsx
@@ -1,18 +1,33 @@
-import { ResizableBox } from "react-resizable";
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import React, { useState, FC } from 'react'
 
 interface ResizableProps {
-    direction: 'e' | 's';
+    direction: 'horizontal' | 'vertical';
 }
 
 const Resizable:FC<ResizableProps> = ({direction, children }) => {
-    
+    let boxProps: ResizableBoxProps
+
+    if (direction === 'horizontal') {
+        boxProps =  {
+            maxConstraints: [window.innerWidth * 0.2, Infinity],
+            minConstraints: [window.innerWidth * 0.75, Infinity],
+            width: window.innerWidth * 0.75,
+            height: Infinity,
+            resizeHandles: ['e']
+          }
+    } else {
+        boxProps = {
+          maxConstraints: [Infinity, window.innerHeight * 0.9],
+          minConstraints: [Infinity, window.innerHeight * 0.05],
+          width: Infinity,
+          height: 500,
+          resizeHandles: ['s']
+        }
+    }
+
     return (
-        <ResizableBox 
-          maxConstraints={[Infinity, window.innerHeight * 0.9]}
-          minConstraints={[Infinity, window.innerHeight * 0.05]}
-          width={Infinity} height={500}
-          resizeHandles={[direction]}>
+        <ResizableBox {...boxProps}>
 
           {children}
 
